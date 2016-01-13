3add3102a56760624b8f95403cc65cc93da615be
Daniel Klein <danhirschklein@gmail.com>
2016-01-13 12:04:12 -0700

    Added Mills' solutionwhich doesn't use .reverse() or Arrays

--- palindrome.js
+++ palindrome.js
@@ -31,10 +31,45 @@ function palindrome (str) {
 };
 
 
-// Others solution
+// Other solutions
+
+//Mills Solution
 
 for (var i = array1.length-1 i >=0; i--)
   array2.push(array1[i]) ??
+
+
+//Mills solution: Mills created solution that doesn't require
+//turning string into array
+// * Description: This function performs according to these requirements:
+// *    1) Takes a single argument, a string
+// *    2) Returns true or false if the word is a palindrome
+// *    3) Ignores spaces and capitalization of the words
+// ************************************************************************/
+console.log(palindromeChecker("A nut for a jar of tuna"));//Returns true
+console.log(palindromeChecker("not a palindrone"));//Returns false
+
+//Declare function that takes a string as a parameter
+function palindromeChecker (string) {
+
+    //Assigning string a new string that is all uppercase
+    string = string.toUpperCase();
+    //Replaces whitespace from string with an empty string
+    string = string.replace(/\s+/g, '');//Removes whitespace
+    //Declaring a new string that is empty
+    var newString = "";
+    //Declaring a for loop, in loop a vairable called i is declared
+    //i is assigned to the (length of string - 1) for indexing purposes
+    //At every iteration i is subtracted by one
+    for (var i = string.length - 1; i >= 0; i--) {
+        //The character at index i of string is added to newString
+        newString = newString + string[i];  
+    }
+
+    //Returns true if the two string are equal
+    return newString === string;//Returns true if palindrome
+
+}
 
 
 //Instructions: Figure out if string is palindrome without using .reverse()
