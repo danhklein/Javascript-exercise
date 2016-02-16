e0ea3491e062ccd2b66ee282c90511c66cc07451
Daniel Klein <danhirschklein@gmail.com>
2016-02-16 09:38:19 -0700

    Added warmup prompt

--- Xoxo.js
+++ Xoxo.js
@@ -1,3 +1,5 @@
+//Xs and 0s: Create a function that takes a string of "x"s and "o"s as inputs. Return True if the number of "x"s and "o"s is even - and False otherwise.
+
 var Xox = function (input) {
   var test = input;
   var xCounter = 0;
